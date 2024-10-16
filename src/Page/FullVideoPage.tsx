c6989d893556ea3371a4fc1e3296b1bfa35a8ad6
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2024-10-16 17:12:41 +0530

    update

--- src/Page/FullVideoPage.tsx
+++ src/Page/FullVideoPage.tsx
@@ -4,7 +4,15 @@ import ImageBox from "../Component/ImageBox";
 
 const FullVideoPage = () => {
   return (
-    <div style={{ height: "100vh" }}>
+    <div
+      style={{
+        height: "100vh",
+        position: "fixed",
+        top: "0px",
+        minHeight: "100vh",
+        width: "100%",
+      }}
+    >
       <Header />
       <ImageBox />
     </div>
